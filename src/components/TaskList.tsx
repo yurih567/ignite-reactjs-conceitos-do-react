1d3d7cd70cd220ad8ae31dba9045566262b92981
Yuri Henrique <yurih567@gmail.com>
2022-06-26 14:54:00 -0300

    Realizado requisitos para concluir o desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,27 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks(state => [...state, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(state => state.map(task =>
+      task.id === id
+      ? {...task, isComplete: !task.isComplete}
+      : task
+    ))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(state => state.filter(task => task.id !== id))
   }
 
   return (
@@ -33,7 +45,7 @@ export function TaskList() {
 
         <div className="input-group">
           <input 
-            type="text" 
+            type="text"
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     hot: true,
+    port: 9572,
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),  
